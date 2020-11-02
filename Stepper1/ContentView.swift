ceec0b9880e30cbcdcb2d2d73c0a640809529579
sir <terror.neru3@gmail.com>
2020-11-02 19:51:30 +0900

    Initial Commit

---
+++ Stepper1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stepper1
+//
+//  Created by 高橋　龍 on 2020/11/02.
+//  Copyright © 2020 com.litech. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
