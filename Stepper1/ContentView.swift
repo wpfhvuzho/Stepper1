d26a5a85fa6f6e919f29f9d0ff8e20efea3cb528
sir <terror.neru3@gmail.com>
2020-11-03 21:35:00 +0900

    ステッパーの機能の実装

--- Stepper1/ContentView.swift
+++ Stepper1/ContentView.swift
@@ -9,8 +9,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var heisei = 1
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Stepper(value: $heisei, in: 1...31) {
+                Text("平成：　\(self.heisei)年")
+            }
+        .padding()
+            Text("西暦 \(String(heisei + 1988))年")
+        }
+        .font(.largeTitle)
     }
 }
 
